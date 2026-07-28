c28656dd8c767405c114e400030efba406f3e91c
isabellaatx <isabella.d.banse@gmail.com>
2026-07-28 14:43:40 -0500

    planning

---
+++ index.js
@@ -0,0 +1,29 @@
+//some calls needed:
+
+//get value from input
+    //save that input, but only for one session, so that if the user refreshes the page, it will not be saved
+
+//send that value to the bot
+
+//bot function to process the input and generate a response
+    //save that response on page as well as in the db to continue the conversation
+
+//send response back to the user
+
+//area for user to type again for the same conversation
+
+
+
+//stylistic choices i want to make:
+
+// as the words in the reply are being generated, they will appear one by one, as if the bot is typing them out in real time
+
+// the responses scroll down the page, so that the user can see the entire conversation history
+
+// bot speaks very retro, speaks like a generic y2k girl/old barbie esq, like a really cool friend
+
+// bot doesnt answer anything but like gossip, like if i asked it to code something it would be like girl yk i dont know how to do that! lmk if u need advice on something
+
+
+
+
